a16b577c126db5bedeae6e59b1a3e7ba413e0cea
MathisGD <mathis@morpho.xyz>
2023-09-04 01:37:32 +0200

    docs: add comment

--- src/irm/libraries/MathLib.sol
+++ src/irm/libraries/MathLib.sol
@@ -15,12 +15,13 @@ library MathLib {
     int256 private constant LN2_INT = 0.693147180559945309 ether;
 
     /// @dev Returns an approximation of exp.
+    /// @dev Expects input between -ln(2**256) and ln(2**256).
     function wExp(int256 x) internal pure returns (uint256) {
         unchecked {
             // Decompose x as x = q * ln(2) + r with q an integer and -ln(2) < r < ln(2).
             int256 q = x / LN2_INT;
             // Safe unchecked * because |q * LN2_INT| <= x.
-            int256 r = x - q * LN2_INT; 
+            int256 r = x - q * LN2_INT;
 
             // Compute e^r.
             int256 firstTerm = WAD_INT;
