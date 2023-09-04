1f8750c1ebead083f09a681dd0218b7fa77292b4
MathisGD <mathis@morpho.xyz>
2023-09-04 18:08:10 +0200

    test: improve bound testing

--- test/irm/UtilsLibTest.sol
+++ test/irm/UtilsLibTest.sol
@@ -8,7 +8,12 @@ contract UtilsTest is Test {
     using UtilsLib for uint256;
 
     function testBound(uint256 x, uint256 low, uint256 high) public {
-        uint256 expectedRes = (x > high ? high : x) < low ? low : (x > high ? high : x);
-        assertEq(x.bound(low, high), expectedRes);
+        if (x <= high) {
+            if (x >= low) assertEq(x.bound(low, high), x);
+            else assertEq(x.bound(low, high), low);
+        } else {
+            if (low <= high) assertEq(x.bound(low, high), high);
+            else assertEq(x.bound(low, high), low);
+        }
     }
 }
