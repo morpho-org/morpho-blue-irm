412d68183660d5622a25ab8859521803e335bfe8
MathisGD <mathis@morpho.xyz>
2023-09-04 18:33:29 +0200

    test: document testing ranges

--- test/irm/MathLibTest.sol
+++ test/irm/MathLibTest.sol
@@ -13,23 +13,27 @@ contract MathLibTest is Test {
     int256 private constant LN2_INT = 0.693147180559945309 ether;
 
     function testWExp(int256 x) public {
+        // Bound between ln(1e-9) ~ -27 and ln(max / 1e18 / 1e18) ~ 94, to be able to use `assertApproxEqRel`.
         x = bound(x, -27 ether, 94 ether);
         assertApproxEqRel(MathLib.wExp(x), uint256(wadExp(x)), 0.01 ether);
     }
 
     function testWExpSmall(int256 x) public {
+        // Bound between min + ln(2)/2 and ln(1e-18).
         x = bound(x, type(int256).min + LN2_INT / 2, -178 ether);
         assertEq(MathLib.wExp(x), 0);
     }
 
     function testWExpTooSmall(int256 x) public {
+        // Bound between min and min + ln(2)/2 - 1.
         x = bound(x, type(int256).min, type(int256).min + LN2_INT / 2 - 1);
         vm.expectRevert(bytes(ErrorsLib.WEXP_UNDERFLOW));
         assertEq(MathLib.wExp(x), 0);
     }
 
     function testWExpTooLarge(int256 x) public {
-        vm.assume(x >= 178 ether);
+        // Bound between ln(2**256-1) ~ 177 and max.
+        x = bound(x, 178 ether, type(int256).max);
         vm.expectRevert(bytes(ErrorsLib.WEXP_OVERFLOW));
         MathLib.wExp(x);
     }
