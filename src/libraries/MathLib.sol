5738df95a458132b60cc433c3b4cf307fe4d7f23
Rubilmax <rmilon@gmail.com>
2023-11-12 12:41:05 +0100

    fix(wexp): raise upper bound to ln(max / 1e36)

--- src/libraries/MathLib.sol
+++ src/libraries/MathLib.sol
@@ -21,17 +21,18 @@ library MathLib {
     int256 internal constant LN_WEI_INT = -41.446531673892822312 ether;
 
     /// @dev Above this bound, `wExp` is clipped to avoid overflowing when multiplied with 1 ether.
-    /// @dev This upper bound corresponds to: ln(type(uint256).max / 1e36) - ln(2) (scaled by WAD, floored).
-    int256 internal constant WEXP_UPPER_BOUND = 93.859467695000409276 ether;
+    /// @dev This upper bound corresponds to: ln(type(uint256).max / 1e36) (scaled by WAD, floored).
+    int256 internal constant WEXP_UPPER_BOUND = 94.552614875560354586 ether;
 
     /// @dev The value of wExp(`WEXP_UPPER_BOUND`).
-    uint256 internal constant WEXP_UPPER_VALUE = 57716089161559221271663333261689707872312.488815486643994624 ether;
+    uint256 internal constant WEXP_UPPER_VALUE = 115432178323118442717551238386899909037872.77663603861225472 ether;
 
     /// @dev Returns an approximation of exp.
     function wExp(int256 x) internal pure returns (uint256) {
         unchecked {
             // If x < ln(1e-18) then exp(x) < 1e-18 so it is rounded to zero.
             if (x < LN_WEI_INT) return 0;
+            // `wExp` is clipped to avoid overflowing when multiplied with 1 ether.
             if (x >= WEXP_UPPER_BOUND) return WEXP_UPPER_VALUE;
 
             // Decompose x as x = q * ln(2) + r with q an integer and -ln(2)/2 <= r <= ln(2)/2.
