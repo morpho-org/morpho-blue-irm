e17490c72d553c91e37252700f26bd9f2cedcf44
Rubilmax <rmilon@gmail.com>
2023-12-07 10:51:18 +0100

    docs(constants): updated values

--- src/libraries/adaptive-curve/ConstantsLib.sol
+++ src/libraries/adaptive-curve/ConstantsLib.sol
@@ -16,12 +16,12 @@ library ConstantsLib {
     /// @notice Target utilization (scaled by WAD).
     int256 public constant TARGET_UTILIZATION = 0.9 ether;
 
-    /// @notice Initial rate at target per second (scaled by WAD).
-    int256 public constant INITIAL_RATE_AT_TARGET = int256(0.04 ether) / 365 days;
+    /// @notice Initial rate at target per second (scaled by WAD) (4% APR <=> 1% min APR).
+    int256 public constant INITIAL_RATE_AT_TARGET = int256(0.01 ether) * CURVE_STEEPNESS / WAD_INT / 365 days;
 
-    /// @notice Mininimum rate at target per second (scaled by WAD) (0.025% APR).
+    /// @notice Minimum rate at target per second (scaled by WAD) (0.1% APR <=> 0.025% min APR).
     int256 public constant MIN_RATE_AT_TARGET = int256(0.00025 ether) * CURVE_STEEPNESS / WAD_INT / 365 days;
 
-    /// @notice Maximum rate at target per second (scaled by WAD) (1000% APR).
+    /// @notice Maximum rate at target per second (scaled by WAD) (250% APR <=> 1000% max APR).
     int256 public constant MAX_RATE_AT_TARGET = int256(0.01e3 ether) * WAD_INT / CURVE_STEEPNESS / 365 days;
 }
