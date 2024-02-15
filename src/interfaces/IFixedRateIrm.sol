eb954aff96e9893a98dc3eab1a6d605f044cd285
MathisGD <74971347+MathisGD@users.noreply.github.com>
2024-02-15 08:14:58 +0100

    docs: minor fix
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/interfaces/IFixedRateIrm.sol
+++ src/interfaces/IFixedRateIrm.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.0;
 import {IIrm} from "../../lib/morpho-blue/src/interfaces/IIrm.sol";
 import {Id} from "../../lib/morpho-blue/src/interfaces/IMorpho.sol";
 
-/// @title IAdaptiveCurveIrm
+/// @title IFixedRateIrm
 /// @author Morpho Labs
 /// @custom:contact security@morpho.org
 interface IFixedRateIrm is IIrm {
